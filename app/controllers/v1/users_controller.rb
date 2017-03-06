9952949e0e5c06c49205b3805f56bd7e03540810
phcastro <phlcastro@gmail.com>
2017-03-06 05:35:40 +0000

    added role attr to users

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -5,12 +5,13 @@ class V1::UsersController < ApplicationController
     user = User.create!(user_params)
 
     knock_token = Knock::AuthToken.new payload: { sub: user.id }
-    render json: { jwt: knock_token.token }, status: :created
+    render json: { jwt: knock_token.token, role: user.role }, status: :created
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                 :password_confirmation, :role)
   end
 end
